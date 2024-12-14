34d41668c48bcfe714eac433bdf1efb8e4193ae2
Arthur-PREVEL <arthur.prevel@etud.univ-jfc.fr>
2024-12-14 19:42:46 +0100

    changement nb de personnes dans la simulation

--- src/main/java/src/Main.java
+++ src/main/java/src/Main.java
@@ -4,7 +4,7 @@ package src;
 public class Main {
     public static void main(String[] args) {
         //création d'une population de 20 personnes
-        Population pop = new Population(20, 1);
+        Population pop = new Population(38, 1);
         //on souhaite avoir 20 pourcents de malades
         pop.setPopMalade(0.2);
         //on créait la maladie
